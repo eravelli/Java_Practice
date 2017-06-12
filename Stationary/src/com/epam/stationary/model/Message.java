6ba277324341dcd4e9a4c4566b12efcf98b6130b
eravelli <eravelli@gmail.com>
2017-06-12 15:09:29 +0530

    Added and committed

---
+++ Stationary/src/com/epam/stationary/model/Message.java
@@ -0,0 +1,15 @@
+package com.epam.stationary.model;
+
+public class Message {
+private String message;
+
+public String getMessage() {
+	return message;
+}
+
+public void setMessage(String message) {
+	this.message = message;
+}
+ 
+ 
+}
