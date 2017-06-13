c2aac033d629ae136026920e3d5f3a73c4da9976
eravelli <eravelli@gmail.com>
2017-06-13 22:50:22 +0530

    Committed for checking

--- StringAssignment/src/com/epam/practice/test/StringPractice.java
+++ StringAssignment/src/com/epam/practice/test/StringPractice.java
@@ -1,14 +1,88 @@
 package com.epam.practice.test;
 
-import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringPractice {
 	
-	
 	public static void main(String args[]) {
+
+		Scanner keyboard = new Scanner(System.in);
+//		System.out.println("Please Enter the Text ");
+//		String s = keyboard.nextLine();
+		System.out.println("Please Enter Opening/Closing character  :");
+		String s="Sun rises in {This is test} &you& East {this} sdfskafh (it) ppppp youtyty& |you|{ttttttttttttttttttttt}";
+		if(s != null && !s.equals("") && !s.isEmpty()){
+		String startChar = keyboard.nextLine();
+		String endChar = keyboard.nextLine();
+		System.out.println("Opening Character = " + startChar);
+		System.out.println("Closing Character = " + endChar);
+		String regex = startChar + "(.*?)" + endChar;
+
+
+		keyboard.close();
+		Pattern p;
+		if(s.contains(startChar) && s.contains(endChar)){
+		
+		if(startChar.matches("[a-zA-Z0-9]") &&  endChar.matches("[a-zA-Z0-9]")){
+			p = Pattern.compile(regex);
+		}else {
+			p = Pattern.compile("\\"+ startChar + "(.*?)" + "\\"+ endChar);
+		}
+
+	    Matcher m = p.matcher(s);
+	    int len = s.length();
+		String removeString = "";
+
+		while (m.find())
+
+		{
+			if (m.group(1).length() < len) {
+				len = m.group(1).length();
+				removeString = m.group(1);
+			}
+		}
+
+		System.out.println("Shortest String = " + removeString);
+		String removeStr = startChar + removeString + endChar;
+
+		System.out.println("Remove String = " + removeStr);
+
+	  /*  while (matcher.find()){
+	    	
+	    	System.out.println(matcher.group());
+	    	System.out.println(matcher.group(0));
+	    	System.out.println(matcher.group(1));
+	    	String key = matcher.group(1);
+	    	String value = matcher.group(0);
+	    	regexMatcher.put(key, value);	
+	    }
+	    List<String> keys = new ArrayList<>();
+	    keys.addAll(regexMatcher.keySet());
+	    Collections.sort(keys, new StringLengthComparator());
+	    System.out.println(regexMatcher.get(keys.get(0)));
+	    System.out.println(keys.get(0));
+//	    s.replace(regexMatcher.get(keys.get(0)), keys.get(0));
+*/	    
+		String manipString = null;
+	    if(startChar.matches("[a-zA-Z0-9]")){
+	    	manipString = s.replaceAll(removeStr, (startChar+endChar));
+		}else{
+			manipString = s.replaceAll("\\p{Punct}|\\d"+removeStr, (startChar+endChar));
+		}
+	    
+	    System.out.println(manipString);
+	    
+		}
+		else{
+			System.out.println("Opening/Closed Character is not available in given text");
+		}
+		}
+
+	}
+	/*public static void main(String args[]) {
 
 		Scanner keyboard = new Scanner(System.in);
 //		System.out.println("Please Enter the Text ");
@@ -49,70 +123,15 @@ public class StringPractice {
 		System.out.println(" String final =" + manipString);
 		}
 
-	}
-
-	/*public static StringBuilder removechar(String string,Character element){
-	 	
-	 	char[] arr = string.toCharArray();
-	 	//blah blah,i used one string function,so what?
-	 	ArrayList<Character> arrlist = new ArrayList<Character>();
-		for(int i = 0;i<=string.length()-1;i++){
-			arrlist.add(((Character)arr[i]));
- 
-		}
- 
-		for(int j = 0;j<=arrlist.size()-1;j++){
-			if(arrlist.get(j).equals(element)){
-				arrlist.remove(j);
-				//remove here is an arraylist function
-			}
-		}
- 
-		StringBuilder result = new StringBuilder(arrlist.size());
-		//StringBuilder is not a function,so shut ur trap
-		for(Character k:arrlist){
-			result.append(k);
-		}
-		return result;
-	}
-	public static void main(String args[]){
-		System.out.print(removechar("penicillin",'i'));
 	}*/
-	
-	
-	/*public static void main(String arg[]){
-		
-		String textMessage = "creating java string by new keyword";
-		
-			String a;
-	        String s;
-	        Scanner scan = new Scanner(System.in);
 
-	        System.out.println("enter a first character");
-	        a = scan.nextLine();
-	        
-	        if(!textMessage.contains(a)){
-	        	 System.out.println("Please re enter avaiabale character");
-	        	 a = scan.nextLine();
-	        }
-	       
-	        System.out.println("enter a second character");
-	        s = scan.nextLine();
-	        if(!textMessage.contains(s)){
-	        	 System.out.println("Please re enter avaiabale character");
-	        	 s = scan.nextLine();
-	        }
-	        
-	       int start= textMessage.indexOf(a);
-	       
-	       int end= textMessage.indexOf(s)+1;
-	        
-	        
-	       StringBuilder sb = new  StringBuilder(textMessage);	        
-		
-	       sb.delete(start, end);
-	       
-	       System.out.println("Remaining text message:::" + sb.toString());
-	}*/
+	
 	
 }
+class StringLengthComparator implements Comparator<String>{ 
+
+    @Override
+        public int compare(String str1, String str2) {
+            return str1.length() - str2.length();
+        }
+ }
